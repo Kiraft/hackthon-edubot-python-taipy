12bc04321ca42ad48111d66d39c5a8f384ab49f5
Kiraft <77902620+Kiraft@users.noreply.github.com>
2024-05-17 22:23:54 -0600

    ultimo para subir

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from taipy.gui import Gui
 from routes.routes import routes
     
-def iniciar_app():
-    Gui(pages=routes, css_file="main.css").run(debug=True, dark_mode=True, use_reloader=True, title="Inicio de Sesión", stylekit=True,)
+Gui(pages=routes, css_file="main.css").run(debug=True, dark_mode=True, use_reloader=True, title="Inicio de Sesión", stylekit=True,)
 
